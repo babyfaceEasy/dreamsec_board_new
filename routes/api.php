b2263024fa71d59253f879c50b400628fec288e5
babyfaceEasy <oodegbaro@gmail.com>
2018-01-25 17:58:34 +0100

    Listings

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('test-api',  function(){
+  return 'olakunle - api';
+});
 
 Route::post('login', 'API\PassportController@login');
 Route::post('register', 'API\PassportController@register');
@@ -27,11 +30,11 @@ Route::group(['middleware' => 'auth:api'], function(){
 
   //AdminController
   Route::post('db-users-count', 'API\AdminController@totalDBClients');
-  Route::post('list-of-users', 'API\AdminController@listOfClients');
+  Route::get('list-of-users', 'API\AdminController@listOfClients');
   Route::get('user/{client_id}/details', 'API\AdminController@clientFullDetails');
   Route::get('user/{query}/search', 'API\AdminController@clientSearch');
   Route::get('client-panic-coordinates', 'API\AdminController@allClientDangerOccurencesCoordinates');
-  Route::get('user/{client_id}/panic-details', 'API\AdminController@clientDangerOccurencesDetails');
+  Route::get('euser/{client_id}/panic-details', 'API\AdminController@clientDangerOccurencesDetails');
   Route::get('user/{client_id}/panic-count', 'API\AdminController@clientDangerOccurencesCount');
   Route::get('user/{client_id}/panic-coordinates', 'API\AdminController@clientDangerOccurencesCoordinates');
 });
